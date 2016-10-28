557342bc016d460b2ab3f24350e644ddb423cd81
Savata <sava.ivanov.9@gmail.com>
2016-10-28 22:44:47 +0300

    Structural Patterns Homework - composite pattern added

---
+++ Topics/04. Structural-Design-Patterns/homework/Composite/CompositePattern/Composite-RealWorld/DrawingElement.cs
@@ -0,0 +1,22 @@
+﻿namespace Composite_RealWorld
+{
+    /// <summary>
+    /// The 'Component' Treenode
+    /// </summary>
+    abstract class DrawingElement
+    {
+        protected string _name;
+
+        // Constructor
+        public DrawingElement(string name)
+        {
+            this._name = name;
+        }
+
+        public abstract void Add(DrawingElement d);
+
+        public abstract void Remove(DrawingElement d);
+
+        public abstract void Display(int indent);
+    }
+}
