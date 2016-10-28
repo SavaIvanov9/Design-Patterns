73d22f0a1576f329e8650793ed93c5195c593766
Savata <sava.ivanov.9@gmail.com>
2016-10-28 23:27:13 +0300

    decorator pattern code

---
+++ Topics/04. Structural-Design-Patterns/homework/Decorator/DecoratorPattern/DecoratorPattern/ConcreteComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPattern
+{
+    /// <summary>
+    /// The 'ConcreteComponent' class
+    /// </summary>
+    public class ConcreteComponent : Component
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("ConcreteComponent.Operation()");
+        }
+    }
+}
---
+++ Topics/04. Structural-Design-Patterns/homework/Decorator/DecoratorPattern/DecoratorPattern/ConcreteDecoratorB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPattern
+{
+    /// <summary>
+    /// The 'ConcreteDecoratorB' class
+    /// </summary>
+    public class ConcreteDecoratorB : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+            AddedBehavior();
+            Console.WriteLine("ConcreteDecoratorB.Operation()");
+        }
+
+        void AddedBehavior()
+        {
+        }
+    }
+}
